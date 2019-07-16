1c73111ef39907b4705b289cce26c2ba13200b96
Raqeeb <rforrizwan55@gmail.com>
2019-07-15 23:43:30 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div onClick={this.props.onClick}>This div has been clicked {this.props.clicks} times.</div>
+      <div onClick={this.props.onClickDiv}>This div has been clicked {this.props.clicks} times.</div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 let model = { clicks: 0 };
 function render() {
   ReactDOM.render(
-    <App clicks={model.clicks} onClick={() => { model.clicks += 1; render(); }} />,
+    <App clicks={model.clicks} onClickDiv={() => { model.clicks += 1; render(); }} />,
     document.getElementById('root')
   );
 }
